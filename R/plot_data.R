64bec6f6ab8cde85dec02d70537e005128aa0aa3
pfgherardini <federico.gherardini@gmail.com>
2018-06-13 16:52:01 -0700

    started refactoring plotting functions

--- R/plot_data.R
+++ R/plot_data.R
@@ -14,10 +14,8 @@ plot_communities <- function(G, att.names, ...) {
 
 
 
-density_scatterplot <- function(tab, x_name, y_name, grouping)
-{
-    m <- ddply(tab, grouping, function(m, x_name, y_name)
-    {
+density_scatterplot <- function(tab, x_name, y_name, grouping) {
+    m <- plyr::ddply(tab, grouping, function(m, x_name, y_name) {
         colramp <- grDevices::colorRampPalette(c("black", "red", "yellow"))
         dens.col <- grDevices::densCols(m[, x_name], m[, y_name], colramp = colramp)
         return(data.frame(m, dens.col = dens.col))
@@ -26,62 +24,93 @@ density_scatterplot <- function(tab, x_name, y_name, grouping)
     maxx <- max(m[, x_name], na.rm = T) + 0.5
     maxy <- max(m[, y_name], na.rm = T) + 0.5
     
-    (p <- ggplot(aes_string(x = x_name, y = y_name, color = "dens.col", size = 1), data = m)
-     + facet_wrap(grouping)
-     + geom_point()
-     + scale_colour_identity() 
-     + scale_size_identity()
-     + xlim(0, maxx)
-     + ylim(0, maxy)
+    (p <- ggplot2::ggplot(ggplot2::aes_string(x = x_name, y = y_name, color = "dens.col", size = 1), data = m)
+            + ggplot2::facet_wrap(grouping)
+            + ggplot2::geom_point()
+            + ggplot2::scale_colour_identity() 
+            + ggplot2::scale_size_identity()
+            + ggplot2::xlim(0, maxx)
+            + ggplot2::ylim(0, maxy)
     )
     
     return(p)
 }
 
 
-plot_cluster <- function(data, clusters, graph.name, col.names, pool.cluster.data, plot.type)
-{
-    G <- data$graphs[[graph.name]]
-    gated_data <- data$landmarks.data
-    clustered_data <- data$clustered.data[[graph.name]]
-    #print(gated_data[1:10,])
-    #print(clustered_data[1:10,])
-    names(clustered_data) <- gsub("^X", "", names(clustered_data))
-    names(gated_data) <- gsub("^X", "", names(gated_data))
+load_rds_data <- function(v, dir) {
+    ret <- lapply(v, function(s) {
+        readRDS(file.path(dir, sprintf("%s.rds", s)))
+    })
+    return(do.call(rbind, ret))
+}
+
+
+add_missing_columns <- function(m, col.names, fill.data) {
+    v <- col.names[!(col.names %in% colnames(m))]
+    ret <- matrix(nrow = nrow(m), ncol = length(v), data = fill.data)
+    colnames(ret) <- v
+    ret <- data.frame(m, ret, check.names = FALSE, stringsAsFactors = FALSE)
+    return(ret)
+}
+
+
+
+plot_scaffold_clusters <- function(clusters, G, graphml.fname, working.dir, col.names, pool.cluster.data, plot.type) {
+    base.name <- gsub(".graphml$", "", graphml.fname)
+    
+    clusters.data <- load_rds_data(clusters, file.path(working.dir, "clusters_data", base.name))
+    clusters.data <- clusters.data[, c(col.names, "cellType")]
+    
+    # Select only the landmark nodes that are connected to these clusters
+    land <- V(G)[nei(V(G)$Label %in% clusters)]$Label
+    land <- V(G)[(V(G)$Label %in% land) & V(G)$type == "landmark"]$Label
+    landmarks.data <- load_rds_data(land, "landmarks_data")
+    
     
     #This only works if the col.names are actually present in the clustered.data
     #TODO: figure out a consistent way to deal with panel mismatches
     
-    common.names <- col.names[(col.names %in% names(clustered_data)) & (col.names %in% names(gated_data))]
-    clustered_data <- clustered_data[, c(col.names, "cellType")]
-    gated_data <- gated_data[, c(common.names, "cellType")]
-    gated_data <- scaffold:::add_missing_columns(gated_data, col.names, fill.data = NA)
-    #Select only the landmark nodes that are connected to these clusters
-    land <- V(G)[nei(V(G)$Label %in% clusters)]$Label
-    land <- V(G)[(V(G)$Label %in% land) & V(G)$type == 1]$Label
-    temp <- gated_data[gated_data$cellType %in% land,]
-    clus.num <- as.numeric(gsub("c", "", clusters))
-    temp.clustered <- clustered_data[clustered_data$cellType %in% clus.num, ]
+    common.names <- col.names[(col.names %in% names(clusters.data)) & (col.names %in% names(landmarks.data))]
+    landmarks.data <- landmarks.data[, c(common.names, "cellType")]
+    landmarks.data <- add_missing_columns(landmarks.data, col.names, fill.data = NA)
+    
     if(pool.cluster.data)
-        temp.clustered$cellType <- "Clusters"
-    temp <- rbind(temp, temp.clustered)
+        clusters.data$cellType <- "Clusters"
+    
+    temp <- rbind(clusters.data, landmarks.data)
+    
     p <- NULL
+    
     if(plot.type == "Scatterplot")
-    {
         p <- density_scatterplot(temp, x_name = col.names[1], y_name = col.names[2], grouping = "cellType")
-    }
-    else
-    {
-        temp <- melt(temp, id.vars = "cellType")
+    
+    else {
+        temp <- reshape::melt(temp, id.vars = "cellType")
         temp$variable <- as.factor(temp$variable)
         if(plot.type == "Density")
-            p <- ggplot(aes(x = value, color = cellType), data = temp) + geom_density() + facet_wrap(~variable, scales = "free")
+            p <- (ggplot2::ggplot(ggplot2::aes(x = value, color = cellType), data = temp) 
+                    + ggplot2::geom_density() 
+                    + ggplot2::facet_wrap(~variable, scales = "free"))
+        
         else if(plot.type == "Boxplot")
-            p <- ggplot(aes(x = variable, fill = cellType, y = value), data = temp) + geom_boxplot()
+            p <- (ggplot2::ggplot(ggplot2::aes(x = variable, fill = cellType, y = value), data = temp) 
+                    + ggplot2::geom_boxplot())
     }
     return(p)
 }
 
+run_test <- function() {
+    setwd("C:/Users/fgherardini/temp/scaffold_demo/scaffold_result")
+    fname <- "A_cells_found_normalized_A_cells_found_normalized.fcs - A_cells_found_normalized.fcs_Cells.fcs.clustered.txt.graphml"
+    G <- igraph::read.graph(fname, format = "graphml")
+    scaffold2:::plot_scaffold_clusters(c("c1", "c2", "c3"), G, fname, "./", col.names = c("CD3", "CD45", "CD19", "CD14"), F, "Boxplot")
+                           
+    
+    
+    
+    
+    
+}
 
 
 
