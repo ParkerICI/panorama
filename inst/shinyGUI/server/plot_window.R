e4a29b98141f2987bda7e109f116750f9e4689e5
pfgherardini <federico.gherardini@gmail.com>
2018-06-26 17:13:54 -0700

    cleanup

--- inst/shinyGUI/server/plot_window.R
+++ inst/shinyGUI/server/plot_window.R
@@ -1,9 +1,7 @@
 render_plot_window <- function() { renderUI({
     fluidPage(
         # This window is managed outside of shiny in plotwindow.js
-        tags$head(tags$script(src = "plotwindow.js")),
-        p("Hello world!")
-        
+        tags$head(tags$script(src = "plotwindow.js"))
         
     )
         
