914b1852413f309ee4938b21df5c7e1c10fbbca0
pfgherardini <federico.gherardini@gmail.com>
2018-07-03 15:21:50 -0700

    refactored and documented plot_clusters

--- R/plot_data.R
+++ R/plot_data.R
@@ -71,12 +71,36 @@ load_clusters_data <- function(clusters, samples, dir.prefix) {
 
 #samples.to.plot need to be specified if pool.samples = F, and this is a multi files clustering
 #pool clusters implies pool samples
-plot_clusters <- function(G, clusters, working.dir, col.names, pool.cluster.data, plot.type, pool.samples = TRUE, samples.to.plot = NULL, facet.by = "sample") {
+
+#' Plot clusters data
+#' 
+#' This function plots data for individual clusters and landmarks and has different behaviours depending 
+#'   on whether the clusters represent individual or multiple samples (i.e. wether they were generated using
+#'   \code{scfeatures::cluster_fcs_files} or \code{scfeatures::cluster_fcs_files_groups})
+#' 
+#' @param G An \code{igraph} graph object
+#' @param clusters A numeric vector representing the indices of the vertices of \code{G} to plot (i.e. the clusters)
+#' @param col.names A character vector of variables to plot (these must correspond to the columns in the original clusters data)
+#' @param working.dir The working directory. Must contain two subdirectories named \code{clusters_data} and \code{landmarks_data}. The latter
+#'   is only necessary if \code{G} contains nodes of type \code{landmark} (i.e. it represents a Scaffold map)
+#' @param plot.type Either \code{"Boxplot"} or \code{"Scatterplot"}. The type of plot
+#' @param pool.clusters Whether to pool the clusters data. If this is \code{FALSE} each cluster is plotted separately
+#' @param pool.samples Whether to pool data from different samples. If this is \code{FALSE} each sample is plotted separately. Note that \code{pool.clusters}
+#'   take precedence over this (i.e. if the clusters are pooled, the samples will be pooled too, irrespective of the value of this option)
+#' @param samples.to.plot The samples to be plotted. This option is only used if \code{pool.samples == FALSE} and \code{G} does not have 
+#'   a \code{sample} vertex property (i.e. the vertices were generated using \code{scfeatures::cluster_fcs_files_groups}, and as such they represent
+#'   multiple samples). In such cases this option is mandatory. Otherwise, if the nodes (i.e. the clusters) represent single samples
+#'   the samples to be plotted are selected from the \code{sample} vertex property of the corresponding nodes
+#' @param facet.by In cases where multiple samples are plotted, whether the plots should be faceted by \code{"sample"} or by \code{"variable"}
+#' 
+#' @return Returns a \code{ggplot2} plot object
+#' 
+plot_clusters <- function(G, clusters, col.names, working.dir, plot.type, pool.clusters = FALSE, pool.samples = FALSE, samples.to.plot = NULL, facet.by = "sample") {
  
     cl.labels <- V(G)$Label[clusters]
     clusters.data <- NULL
     
-    if(is.null(V(G)$sample) && is.null(samples.to.plot)) # Plot the pooled data
+    if(is.null(V(G)$sample) && is.null(samples.to.plot)) # Load the pooled data
         clusters.data <- load_rds_data(cl.labels, file.path(working.dir, "clusters_data", "pooled"))
     else {
         samples <- NULL
@@ -88,11 +112,10 @@ plot_clusters <- function(G, clusters, working.dir, col.names, pool.cluster.data
         clusters.data <- load_clusters_data(cl.labels, samples, file.path(working.dir, "clusters_data"))
     }
     
-    
     if(pool.samples)
         clusters.data$sample <- NULL
     
-    if(pool.cluster.data) {
+    if(pool.clusters) {
         clusters.data$cellType <- "Clusters"
         clusters.data$sample <- NULL
     }
@@ -105,28 +128,19 @@ plot_clusters <- function(G, clusters, working.dir, col.names, pool.cluster.data
     landmarks.data <- NULL
     
     if(any(V(G)$type == "landmark")) {
-    
         # Select only the landmark nodes that are connected to these clusters
         land <- V(G)[nei(V(G)$Label %in% cl.labels)]$Label
         land <- V(G)[(V(G)$Label %in% land) & V(G)$type == "landmark"]$Label
         landmarks.data <- load_rds_data(land, file.path(working.dir, "landmarks_data"))
         
-        
-        #This only works if the col.names are actually present in the clustered.data
-        #TODO: figure out a consistent way to deal with panel mismatches
-        
         common.names <- intersect(col.names, names(landmarks.data))
         landmarks.data <- landmarks.data[, c(common.names, "cellType")]
         landmarks.data <- add_missing_columns(landmarks.data, col.names, fill.data = NA)
 
-        
         if(!is.null(clusters.data$sample))
             # Duplicate landmarks data for each sample for plotting
             landmarks.data <- data.frame(landmarks.data, sample = rep(unique(clusters.data$sample), each = nrow(landmarks.data)),
                                          check.names = FALSE, stringsAsFactors = FALSE)
-        
-        
-        
     }
 
     temp <- rbind(clusters.data, landmarks.data)
@@ -135,7 +149,6 @@ plot_clusters <- function(G, clusters, working.dir, col.names, pool.cluster.data
     
     if(plot.type == "Scatterplot")
         p <- density_scatterplot(temp, x_name = col.names[1], y_name = col.names[2], grouping = "cellType")
-    
     else 
         p <- expression_plot(temp, plot.type, facet.by)
 
@@ -144,10 +157,8 @@ plot_clusters <- function(G, clusters, working.dir, col.names, pool.cluster.data
 
 
 expression_plot <- function(tab, plot.type, facet.by = "sample") {
-    
     # Add error message that you cannot subset density plots by sample
     p <- NULL
-
     
     if(!is.null(tab$sample)) {
         tab <- reshape::melt(tab, id.vars = c("cellType", "sample"))
@@ -167,7 +178,6 @@ expression_plot <- function(tab, plot.type, facet.by = "sample") {
         
     }
     else {
-    
         tab <- reshape::melt(tab, id.vars = "cellType")
         tab$variable <- as.factor(tab$variable)
         tab$cellType <- as.factor(tab$cellType)
@@ -189,9 +199,9 @@ run_test <- function() {
     setwd("C:/Users/fgherardini/temp/dave_flu/fcs files/scaffold_result/")
     fname <- "group1.graphml"
     G <- igraph::read.graph(fname, format = "graphml")
-    scaffold2:::plot_clusters(G, c(20, 21 , 22), "./", col.names = c("CD3", "CD45", "CD19", "CD14"), T, "Boxplot", pool.samples = T, c("101_3.fcs", "102_4.fcs"))
                            
-    
+    scaffold2:::plot_clusters(G,  c(20, 21 , 22), col.names = c("CD3", "CD45", "CD19", "CD14"), working.dir = "./", plot.type = "Boxplot", 
+                              pool.clusters = F, pool.samples = F, samples.to.plot = c("101_3.fcs", "102_4.fcs"))
     
     
     
